3adfda86d546646e435e68cb79f9f011b172435f
Kyoko <108702599+KyoMatias@users.noreply.github.com>
2023-12-25 19:59:29 +0800

    Adjusted Movement Script
    Done At 8Pm on Christmas Day

--- ActivityOne/Assets/Scripts/MovementScript.cs
+++ ActivityOne/Assets/Scripts/MovementScript.cs
@@ -8,12 +8,19 @@ public class MovementScript : MonoBehaviour
 {
     public float MoveSpeed = 10f;
     public int PlayerStealth;
-    
-    
-// Update is called once per frame
+
+
+    private void Start()
+    {
+        PlayerStealth = 100; // HARDCODED VARIABLE! USED FOR REFERENCES.
+        Debug.Log("$Player Stealth Value: " + PlayerStealth);
+    }
+
+    // Update is called once per frame
     void FixedUpdate()
     { 
         PlayerControls(); // checks for player movement by calling the function in the FixedUpdate.
+        StealthCounter(); // checks for player stealth by its values and how much danger they are in.
     }
 
     void PlayerControls()
@@ -30,19 +37,19 @@ public class MovementScript : MonoBehaviour
 
     void StealthCounter() //Function that checks for player stealth (HARDCODED, FOR REFERENCES)
     {
-        PlayerStealth = 100;
-
-        if (PlayerStealth >= 50)
+        
+        if (PlayerStealth <= 50) //Setup for engage at 50 PS
         {
-            Debug.Log("Warning! Your Stealth is Getting Low, Move away from towers to regenerate");
+            Debug.Log("Warning! Your Stealth is Getting Low, Move away from towers to regenerate"); 
+            //Add Code here for Warning Function [ WarningFunc() ]
         }
         
-        else if (PlayerStealth == 0)
+        else if (PlayerStealth == 0) //Engage PLayerDeath() Function on zero.
         {
-            PlayerDeath();
+            PlayerDeath(); //PlayerDeath Initialized 
         }
     }
-
+    
     private void PlayerDeath()
     {
         GameManager.Instance.GameRestart(true);
